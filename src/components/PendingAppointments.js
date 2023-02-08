63f28a7242eef03e344504d0be255c847e7d4ce9
Allan <kipropsallan@gmail.com>
2023-02-08 12:19:32 +0300

    updated components

---
+++ src/components/PendingAppointments.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PendingAppointments = props => {
+    return (
+      <div className="pendingAppointments">
+        <div className="pendingAppointmentsContainer">
+          <span className="pendingAppointmentsNum">50</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default PendingAppointments;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,39 +2,63 @@ import "@progress/kendo-theme-material/dist/all.css";
 import { TileLayout } from "@progress/kendo-react-layout";
 import {useState, React} from 'react'
 import './Dashboard.css'
+import ActiveUsers from "../components/ActiveUsers";
+import FollowUp from "../components/FollowUp";
+import PendingAppointments from "../components/PendingAppointments";
 
-const WidgetOne = () => <div>Widget one</div>;
-const WidgetTwo = () => <div>Widget two</div>;
+// const WidgetOne = () => <div>Widget one</div>;
+// const WidgetTwo = () => <div>Widget two</div>;
 
 const initialPositions = [
     {
-      col: 1,
-      colSpan: 2,
-      rowSpan: 2,
-    },
-    {
-      col: 3,
-      colSpan: 1,
-      rowSpan: 1,
-    },
+        col: 1,
+        colSpan: 2,
+        rowSpan: 2,
+      },
+      {
+        col: 3,
+        colSpan: 1,
+        rowSpan: 1,
+      },
+      {
+        col: 4,
+        colSpan: 1,
+        rowSpan: 1,
+      },
+      {
+        col: 3,
+        colSpan: 2,
+        rowSpan: 2,
+      },
   ];
 
+  const getPositions = initialPositions => {
+    return (
+        JSON.parse(localStorage.getItem("dashboard-positions")) || initialPositions
+      );
+    };
+
 const Dashboard = () => {
-    const [positions, setPositions] = useState(initialPositions);
+    const [positions, setPositions] = useState(getPositions(initialPositions));
 
     const widgets = [
          {
-        header: "Widget one header",
-        body: <WidgetOne />,
+        header: "Active Users",
+        body: <ActiveUsers />,
       },
       {
-        header: "Widget two header",
-        body: <WidgetTwo />,
+        header: "Appointments",
+        body: <PendingAppointments />,
+      },
+      {
+        header: "Follow Up",
+        body: <FollowUp />,
       },
     ];
   
     const handleReposition = e => {
       setPositions(e.value);
+      localStorage.setItem("dashboard-positions", JSON.stringify(e.value));
     };
 
     return (
@@ -44,8 +68,8 @@ const Dashboard = () => {
             className="tileLayout"
             columns={4}
             rowHeight={255}
-            gap={{ rows: 10, columns: 10 }}
             positions={positions}
+            gap={{ rows: 10, columns: 10 }}
             items={widgets}
             onReposition={handleReposition}
           />
