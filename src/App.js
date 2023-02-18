482cc5518923b751f3573a01bdd174dee6dc2c45
Allan <kipropsallan@gmail.com>
2023-02-18 10:29:30 +0300

    updated components and styling

--- src/App.js
+++ src/App.js
@@ -10,17 +10,16 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
-     <BrowserRouter>
+  <>
+      <BrowserRouter>
         <Routes>
-            <Route path="/" element={<LoginPage/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage/>} />
             <Route path="/signup" element={<SignupPage/>} />
             <Route path="/home" element={<Home/>} />
         </Routes>
       </BrowserRouter>
-    </div>
-  </div>
+  </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,54 @@
-import React from 'react';
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
       {/* Navigation bar */}
-      <nav>
+      <nav className='navBar'>
+
         <p className="about">About</p>
         <p className="signup">Account</p>
         <p className="contact">Contact</p>
       </nav>
-      
+
       {/* Page header */}
-      <header>
-        <h1 className="title">Angaza Counselling</h1>
-        <h2 className="tag">“Genuine feelings cannot be produced, nor can they be eradicated… the body sticks to the facts.”</h2>
-      </header>
-      
-      {/* Call-to-action buttons */}
-      <div className="home_cta1">
-        <button>Appointments</button>
-      </div>
-      <div className="home_cta2">
-      <button>Services Offered</button>
-      </div>
-      
+      <section className="mainContent">
+        <section className="title">
+          <h1>Angaza </h1>
+          <h1> Counselling</h1>
+
+        </section>
+
+        <section className="info">
+          <div>
+            <div className="tag">
+            <h2>
+              “Genuine feelings cannot be produced, nor can they be eradicated…
+              the body sticks to the facts.”
+            </h2>
+            </div>
+            <br/>
+            <br/>
+
+
+            <div className='buttSec'>
+            <div>
+              <button>Appointments</button>
+            </div>
+
+            <div>
+              <button>Services Offered</button>
+            </div>
+            </div>
+
+          </div>
+
+          {/* Call-to-action buttons */}
+
+        </section>
+      </section>
+
       {/* Rest of the page content goes here */}
     </div>
   );
